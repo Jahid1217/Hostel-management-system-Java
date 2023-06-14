0cb884f12e04907b7d2338ef158ae9a3904f7975
Md Jahid Hasan <126156062+Jahid1217@users.noreply.github.com>
2023-06-14 19:24:10 +0600

    Start File

---
+++ Start.java
@@ -0,0 +1,9 @@
+import javax.swing.*;  
+import java.awt.event.*; 
+import LOGINPAGE.*;
+public class Start{
+
+      public static void main(String[] args) {
+            new LoginPage();
+      }
+}
